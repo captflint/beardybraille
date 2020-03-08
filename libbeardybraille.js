efe520c00927e8fff23917716f09e61a4ba668a5
James Stephenson <c4p7.fl1n7@gmail.com>
2020-03-08 17:17:56 -0400

    BrailleCell prototype probably complete

--- libbeardybraille.js
+++ libbeardybraille.js
@@ -8,7 +8,7 @@ function BrailleCell(value) {
 	}
 	
 	this.braille = function() {
-		return String.fromCharCode(0x2800 + this.value);
+		return String.fromCodePoint(0x2800 + this.value);
 	}
 
 	this.setFromArray = function(a) {
@@ -19,6 +19,26 @@ function BrailleCell(value) {
 			}
 		}
 		this.value = sum;
+	}
+
+	this.setFromAscii = function(value) {
+		if (bascii.includes(value)) {
+			this.value = bascii.indexOf(value);
+		} else {
+			throw "Invalid character";
+		}
+	}
+
+	this.setFromUnicode = function(value) {
+		let bunicode = "";
+		for (let i = 0; i < 64; i++) {
+			bunicode += String.fromCodePoint(0x2800 + i);
+		}
+		if (bunicode.includes(value)) {
+			this.value = bunicode.indexOf(value);
+		} else {
+			throw "Invalid character";
+		}
 	}
 
 	if (typeof(value) === "number") {
@@ -34,6 +54,16 @@ function BrailleCell(value) {
 		this.value = value;
 	} else if (Array.isArray(value)) {
 		this.setFromArray(value);
+	} else if (typeof(value) === "string") {
+		if (value.length === 1) {
+			if (value.charCodeAt(0) < 128) {
+				this.setFromAscii(value);
+			} else {
+				this.setFromUnicode(value);
+			}
+		} else {
+			throw "String must contain only a single character";
+		}
 	} else {
 		throw "Invalid input";
 	}
